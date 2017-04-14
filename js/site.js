3b196633d0b58475af749feb537ccf5ef41b0f57
cthib <cortland.thibodeau@gmail.com>
2017-04-13 17:08:21 -0700

    Added video header and instafeed.

---
+++ js/site.js
@@ -0,0 +1,10 @@
+$(function () {
+  var feed = new Instafeed({
+      get: 'user',
+      userId: 538737859,
+      accessToken: '5319357508.c97443f.85534bb0ee23471cb5d5bdab0cfcd91a',
+      limit: 6,
+      resolution: 'low_resolution',
+  });
+  feed.run();
+});
